717a9c0eed87cf0622baa8a75f028e9e0175bd32
Nicolas Schmid <nicolas@breiten.ch>
2021-07-01 12:15:07 +0200

    feat: fix init of js and position of textbox

--- website/src/js/components/InteractiveImage.js
+++ website/src/js/components/InteractiveImage.js
@@ -4,13 +4,18 @@ import InteractiveInfo from './InteractiveInfo';
 
 export default class InteractiveImage {
     constructor(scrollContainer) {
+        this.titleSection = document.querySelector('.fullscreen-image__titlesection');
+        this.fullscreenImage = document.querySelector('.fullscreen-image__img');
+        this.titleVisible = true;
+
         // Initialize Locomotive Scroll (horizontal direction)
         this.lscroll = new LocomotiveScroll({
             el: scrollContainer,
             smooth: true,
             direction: 'horizontal',
-            smartphone: { smooth: true, direction: 'vertical' },
-            tablet: { smooth: true, direction: 'vertical' },
+            smartphone: { smooth: false, direction: 'vertical' },
+            tablet: { smooth: false, direction: 'vertical' },
+            resetNativeScroll: false,
         });
 
         // init all InteractiveInfo objects to array
@@ -21,28 +26,20 @@ export default class InteractiveImage {
             },
             []
         );
-        // hide title section on first scroll event
-        this.titlesection = document.querySelector('.fullscreen-image__titlesection');
-        this.titleVisible = true;
-        this.lscroll.on('scroll', () => {
-            if (this.titleVisible) {
-                this.titlesection.classList.add('hidden');
-                this.titleVisible = false;
-            }
-        });
 
         // add event listener to update position when window is resized
         window.addEventListener('resize', debounce(this.updatePosition, 30));
+
         // set position on first load
         this.updatePosition();
+        this.hideTitleOnScroll();
+        this.hideTitleOnMobile();
     }
 
     // update position relative to image width
     updatePosition = () => {
         // get images bounding rect
-        const { height, width } = document
-            .querySelector('.fullscreen-image__img')
-            .getBoundingClientRect();
+        const { height, width } = this.fullscreenImage.getBoundingClientRect();
 
         // update each info box position
         this.info.forEach((infoElement) => {
@@ -52,5 +49,28 @@ export default class InteractiveImage {
             infoElement.element.style.left = newLeft;
             infoElement.element.style.top = newTop;
         });
+    };
+
+    hideTitleOnScroll = () => {
+        // hide title section on first scroll event
+        this.lscroll.on('scroll', ({ scroll }) => {
+            if (scroll.x < 100 && !this.titleVisible) {
+                this.titleSection.classList.remove('hidden');
+                this.titleVisible = true;
+            } else if (scroll.x >= 100 && this.titleVisible) {
+                this.titleSection.classList.add('hidden');
+                this.titleVisible = false;
+            }
+        });
+    };
+
+    hideTitleOnMobile = () => {
+        document.ontouchend = () => {
+            if (this.titleVisible) {
+                this.titleSection.classList.add('hidden');
+                this.titleVisible = false;
+            }
+            console.log(this.fullscreenImage.getBoundingClientRect());
+        };
     };
 }
