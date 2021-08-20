1ca689e19cab08b94b5384ba6862f11f83e566d0
Nicolas Schmid <nicolas@breiten.ch>
2021-08-20 09:37:58 +0200

    fix: add some layer and bugfixes to hauptthemen

--- website/src/js/components/InteractiveImage.js
+++ website/src/js/components/InteractiveImage.js
@@ -8,11 +8,13 @@ export default class InteractiveImage {
         this.weitergedacht = document.querySelector('.fullscreen-image__weitergedacht');
         this.weitergedachtPointer = document.querySelector('#pointer-weitergedacht');
         this.fullscreenImage = document.querySelector('.fullscreen-image__img');
+        this.annotations = [...document.getElementsByClassName('annotations__element')];
         this.titleVisible = true;
         this.mobileLocomotive = {};
+        this.isTouch = window.matchMedia('not all and (hover: hover), not all and (pointer: fine)');
 
         // small hack to use locomotive on desktop screens < 1024
-        if (window.matchMedia('not all and (hover: hover), not all and (pointer: fine)').matches) {
+        if (this.isTouch.matches) {
             this.mobileLocomotive = {
                 smartphone: { smooth: false, direction: 'vertical' },
                 tablet: { smooth: false, direction: 'vertical' },
@@ -41,19 +43,17 @@ export default class InteractiveImage {
             []
         );
 
-        this.annotations = [...document.getElementsByClassName('annotations__element')];
-        this.annotations.forEach((annot) => {
-            annot.style.transform = `rotate(${annot.dataset.rot}deg)`;
-        });
-
+        // set position on first load
+        this.updatePosition();
         // add event listener to update position when window is resized
         window.addEventListener('resize', debounce(this.updatePosition, 100));
 
-        // set position on first load
-        this.updatePosition();
-        this.hideTitleOnScroll();
-        this.hideTitleOnMobile();
+        // set up event listeners for 'weitergedacht'
         this.showWeitergedacht();
+
+        // set up event listener to hide Title
+        if (this.isTouch.matches) this.hideTitleOnMobile();
+        else this.hideTitleOnScroll();
     }
 
     // update position relative to image width
@@ -74,10 +74,14 @@ export default class InteractiveImage {
         this.annotations.forEach((annot) => {
             annot.style.left = `${(annot.dataset.posx / 100) * width}px`;
             annot.style.top = `${(annot.dataset.posy / 100) * height}px`;
+            annot.style.transform = `rotate(${annot.dataset.rot}deg)`;
         });
 
+        // update weitergedacht position
         const weiterLeft = `${width - this.weitergedacht.getBoundingClientRect().width}px`;
         this.weitergedacht.style.left = weiterLeft;
+
+        // update weitergedacht weitergedacht pointer position
         this.weitergedachtPointer.style.left = `calc(${width - 200}px`;
         this.weitergedachtPointer.style.bottom = `90px`;
     };
@@ -96,10 +100,19 @@ export default class InteractiveImage {
     };
 
     hideTitleOnMobile = () => {
+        const menu = document.getElementById('toggleMenu');
+        const background = document.querySelector('.menu__background');
+
+        if (window.innerWidth < 800) {
+            menu.style.opacity = 0.4;
+            background.style.opacity = 0.0;
+        }
         document.ontouchend = () => {
             if (this.titleVisible) {
                 this.titleSection.classList.add('hidden');
                 this.titleVisible = false;
+                menu.style.opacity = 1;
+                background.style.opacity = 1;
             }
         };
     };
